aa5b1a8f3ed9fcf7b6ce83cb3b40d1bdfcaba948
jonschlinkert <dev@sellside.com>
2014-08-27 03:32:41 -0400

    v0.2.0
    Adds `.parse` and `.parseSync` methods

--- index.js
+++ index.js
@@ -4,17 +4,9 @@
  * Module dependencies.
  */
 
-var fs = require('fs');
 var _ = require('lodash');
-var glob = require('globby');
 var matter = require('gray-matter');
 var utils = require('parser-utils');
-var async = require('async');
-
-
-function arrayify(val) {
-  return Array.isArray(val) ? val : [val];
-}
 
 
 /**
@@ -25,49 +17,41 @@ var parser = module.exports;
 
 
 /**
- * Parse the given `str` and return a `file` object.
+ * Parse the given `file` into a normalized `file` object and callback `next(err, file)`.
+ * Options are passed to [gray-matter], and if `options` has a `locals` property, it
+ * will be merged with the `data` property on the normalized `file` object.
  *
- * @param {String} `str` The string to parse.
- * @param {Object} `options` to pass to [gray-matter].
- * @api public
- */
-
-parser.parseSync = function parseSync(str, options) {
-  var opts = _.extend({}, options);
-
-  try {
-    var file = utils.extendFile(str, options);
-    _.merge(file, matter(file.content, opts));
-
-    return file;
-  } catch (err) {
-    return err;
-  }
-};
-
-
-/**
- * Parse the given `str` callback `next(err, file)`.
+ * Normalized `file` objects should have the following properties:
  *
- * @param {String} `str` The string to parse.
+ *   - `path` The source file path, if provided
+ *   - `data`: metadata, from yaml front matter and/or locals
+ *   - `content`: the content of a file, excluding front-matter
+ *   - `orig`: the original content of a file, including front-matter
+ *
+ * @param {String|Object} `file` The object or string to parse.
  * @param {Object|Function} `options` or `next` callback function.
  * @param {Function} `next` callback function.
  * @api public
  */
 
-parser.parse = function parse(str, options, next) {
+parser.parse = function matterParse(file, options, next) {
   if (typeof options === 'function') {
     next = options;
     options = {};
   }
 
   var opts = _.extend({}, options);
+  var o = {};
 
+  if (typeof file === 'string') {
+    o.content = file;
+  } else {
+    o = file;
+  }
+
+  var res = matter(o.content, opts);
   try {
-    var file = utils.extendFile(str, options);
-    _.merge(file, matter(file.content, opts));
-
-    next(null, file);
+    next(null, utils.extendFile(res, opts));
   } catch (err) {
     next(err);
     return;
@@ -76,97 +60,28 @@ parser.parse = function parse(str, options, next) {
 
 
 /**
- * Read a file at the given `filepath`, passing the resulting
- * string and `options` to the `.parse()` method.
+ * Parse the given `file` and return a normalized `file` object,
+ * with `data`, `content`, `path` and `orig` properties.
  *
- * @param {String} `filepath`
- * @param {Object} `options` to pass to [gray-matter]
- * @return {Object} Parsed `file` object.
+ * @param {String|Object} `file` The object or string to parse.
+ * @param {Object} `options` to pass to [gray-matter].
  * @api public
  */
 
-parser.parseFileSync = function parseFileSync(filepath, options) {
-  var str = fs.readFileSync(filepath, 'utf8');
-  return parser.parseSync(str, options);
-};
-
-
-/**
- * Asynchronously read a file at the given `filepath` and
- * callback `next(err, str)`.
- *
- * @param {String} `path`
- * @param {Object|Function} `options` or next function.
- * @param {Function} `next`
- * @api public
- */
-
-parser.parseFile = function parseFile(filepath, options, next) {
-  if (typeof options === 'function') {
-    next = options;
-    options = {};
-  }
-
+parser.parseSync = function matterParseSync(file, options) {
   var opts = _.extend({}, options);
+  var o = {};
+
+  if (typeof file === 'string') {
+    o.content = file;
+  } else {
+    o = file;
+  }
 
   try {
-    var str = fs.readFileSync(filepath, 'utf8');
-    parser.parse(str, opts, next);
+    var res = matter(o.content, opts);
+    return utils.extendFile(res, opts);
   } catch (err) {
-    next(err);
-    return;
+    return err;
   }
-};
-
-
-/**
- * Synchronously read and parse a glob of files from the given `patterns`,
- * passing any options to [globby] and `.parseSync()`.
- *
- * @param {String} `filepath`
- * @param {Object} `options` to pass to [gray-matter]
- * @return {Object} Parsed `file` object.
- * @api public
- */
-
-parser.parseFilesSync = function parseFilesSync(patterns, options) {
-  return glob.sync(patterns, options).map(function (filepath) {
-    return parser.parseFileSync(filepath, options);
-  });
-};
-
-
-/**
- * Synchronously read and parse a glob of files from the given `patterns`
- * and callback `next(err, str)`. Options are passed to [globby] and `.parseSync()`.
- *
- * @param {String} `path`
- * @param {Object|Function} `options` or next function.
- * @param {Function} `next`
- * @api public
- */
-
-parser.parseFiles = function parseFiles(patterns, options, next) {
-  if (!patterns.length) {
-    next(null, []);
-    return;
-  }
-
-  if (typeof options === 'function') {
-    next = options;
-    options = {};
-  }
-
-  glob(arrayify(patterns), options, function (err, files) {
-    if (err) {
-      next(err);
-      return;
-    }
-
-    async.parallel(files.map(function (file) {
-      return function (cb) {
-        parser.parseFile(file, cb);
-      };
-    }), next);
-  });
 };
