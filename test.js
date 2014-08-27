7b0be6a5b51e82fc0dbd1f2ff3a7c35dcf4ea845
jonschlinkert <dev@sellside.com>
2014-08-27 03:32:41 -0400

    v0.2.0
    Adds `.parse` and `.parseSync` methods

---
+++ test.js
@@ -0,0 +1,61 @@
+/*!
+ * parser-cache <https://github.com/jonschlinkert/parser-cache>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var should = require('should');
+var parser = require('./');
+
+describe('parsers', function() {
+
+  describe('.parseSync()', function() {
+    it('should parse a string.', function() {
+      var o = parser.parseSync('abc');
+      o.should.have.property('data');
+      o.should.have.property('content');
+      o.content.should.equal('abc');
+    });
+
+    it('should parse the content property on an object.', function() {
+      var o = parser.parseSync({content: 'abc'});
+      o.should.have.property('data');
+      o.should.have.property('content');
+      o.content.should.equal('abc');
+    });
+  });
+
+  describe('.parse()', function() {
+    it('should parse a string.', function(done) {
+      parser.parse('abc', function (err, file) {
+        if (err) {
+          done(err);
+        }
+
+        file.should.have.property('data');
+        file.should.have.property('content');
+        file.content.should.equal('abc');
+        done();
+      });
+    });
+
+    it('should parse the content property on an object.', function(done) {
+      parser.parse({content: 'abc'}, function (err, file) {
+        if (err) {
+          done(err);
+        }
+
+        file.should.have.property('data');
+        file.should.have.property('content');
+        file.content.should.equal('abc');
+        done();
+      });
+    });
+  });
+
+});
